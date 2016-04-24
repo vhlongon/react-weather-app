27301df10f37c9c6f29da785eb2b9a8d336ffbcd
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-04-24 21:40:09 +0200

    add redux-promise package

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,1 +1,3 @@
+//Example call: http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=4ed7ee6678b89f4d70ae77e7c4a2f0be
+
 const API_KEY = '4ed7ee6678b89f4d70ae77e7c4a2f0be';
--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,14 @@ import React, {Component} from 'react';
 // redux specific stuff to create a provider and store
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 //import the component and the reducers
 import App from './components/app'
 
 import reducers from './reducers';
+
+//const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 //import your styles
 import css from '../scss/styles.scss';
