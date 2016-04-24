621119f912546b063a5326b5e97dedfe5700cea4
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-04-24 21:52:35 +0200

    build url to make request for the weather widget

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,3 +1,19 @@
-//Example call: http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=4ed7ee6678b89f4d70ae77e7c4a2f0be
+//Example call for the weather app:
+// http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=4ed7ee6678b89f4d70ae77e7c4a2f0be
 
 const API_KEY = '4ed7ee6678b89f4d70ae77e7c4a2f0be';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const COUNTRY_CODE = 'us';
+
+// instead of passing the string directly into the action creator we store it on a variable
+// that we both export (to the reducer) and pass to the action creator,
+// in this way we garantee consistency and avoid bugs because of typos and such
+export const FETCH_WEATER = 'FETCH_WEATER';
+
+export function fetchWeather(city) {
+  let url =  `${ROOT_URL}&q=${city},${COUNTRY_CODE}`;
+  return {
+    type: FETCH_WEATER,
+    url: url
+  };
+}
