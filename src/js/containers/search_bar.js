46859c5e8bc6c264f125ac96515869eaa038b192
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-04-24 21:14:10 +0200

    set searchbar value to state and hook a function on change event

--- src/js/containers/search_bar.js
+++ src/js/containers/search_bar.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //set initial value for the input
+    this.state = { term: ''};
+  }
+
+  onInputChange = (event) => {
+    this.setState({term: event.target.value});
+  }
+
   render() {
     return (
       <form>
-        <input type="text" />
+        <input type="text"
+          placeholder="Get a five-day forecast in your favourite cities"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <button type="submit">Submit</button>
       </form>
     );
